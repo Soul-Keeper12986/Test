72753583a57f2f96a631a5136c10352c99ad9dca
Soul-Keeper12986 <71217987+Soul-Keeper12986@users.noreply.github.com>
2024-02-16 20:15:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-prev = 0
-print("Введите числа через Enter, для завершения ввода, введите 0")
+def max_sum_digits():
+    max_sum = -1
+    max_nums = []
 
-while next != 0:
-    next = abs(sum(map(int, str(abs(int(input()))))))
-    if next > prev:
-        prev = next
+    print("Введите целые числа через Enter, для завершения ввода, введите 0")
 
-print("Наибольшая сумма цифр в числе", prev)
-input()
+    while True:
+        try:
+            num = int(input())
+            if num == 0:
+                break
+            digits_sum = sum([int(digit) for digit in str(abs(num))])
+            if digits_sum > max_sum:
+                max_sum = digits_sum
+                max_nums = [num]
+            elif digits_sum == max_sum:
+                max_nums.append(num)
+
+        except ValueError:
+            print("Неподходящий тип данных, используйте целые числа")
+
+    if len(max_nums) == 0:
+        print("Нет введенных значений")
+
+    if len(max_nums) > 1:
+        print("Наибольшая сумма цифр одинакова в числах", ', '.join(map(str, max_nums)))
+    elif len(max_nums) == 1:
+        print("Наибольшая сумма цифр в числе", ', '.join(map(str, max_nums)))
+
+
+if __name__ == "__main__":
+    max_sum_digits()
+    input()
+    print("Для продолжения ввода перезапустите программу")
+    input()
